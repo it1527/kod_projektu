99483a80884e9e36efeb1b9e0845a2547136712e
it1527 <32055618+it1527@users.noreply.github.com>
2018-10-08 13:59:05 +0200

    Add files via upload
    zprovozneni mqtt na strane esp, zatim bez funkčniho podnetu z mqqt serveru

--- homie-dual-relay/src/main.cpp
+++ homie-dual-relay/src/main.cpp
@@ -13,12 +13,66 @@
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
 HomieNode lightNode("light", "switch");
 
+void onHomieEvent(const HomieEvent& event) {
+  switch (event.type) {
+    case HomieEventType::STANDALONE_MODE:
+      Serial << "Standalone mode started" << endl;
+      break;
+    case HomieEventType::CONFIGURATION_MODE:
+      Serial << "Configuration mode started" << endl;
+      break;
+    case HomieEventType::NORMAL_MODE:
+      Serial << "Normal mode started" << endl;
+      break;
+    case HomieEventType::OTA_STARTED:
+      Serial << "OTA started" << endl;
+      break;
+    case HomieEventType::OTA_PROGRESS:
+      Serial << "OTA progress, " << event.sizeDone << "/" << event.sizeTotal << endl;
+      break;
+    case HomieEventType::OTA_FAILED:
+      Serial << "OTA failed" << endl;
+      break;
+    case HomieEventType::OTA_SUCCESSFUL:
+      Serial << "OTA successful" << endl;
+      break;
+    case HomieEventType::ABOUT_TO_RESET:
+      Serial << "About to reset" << endl;
+      break;
+    case HomieEventType::WIFI_CONNECTED:
+      Serial << "Wi-Fi connected, IP: " << event.ip << ", gateway: " << event.gateway << ", mask: " << event.mask << endl;
+      break;
+    case HomieEventType::WIFI_DISCONNECTED:
+      Serial << "Wi-Fi disconnected, reason: " << (int8_t)event.wifiReason << endl;
+      break;
+    case HomieEventType::MQTT_READY:
+      Serial << "MQTT connected" << endl;
+      break;
+    case HomieEventType::MQTT_DISCONNECTED:
+      Serial << "MQTT disconnected, reason: " << (int8_t)event.mqttReason << endl;
+      break;
+    case HomieEventType::MQTT_PACKET_ACKNOWLEDGED:
+      Serial << "MQTT packet acknowledged, packetId: " << event.packetId << endl;
+      break;
+    case HomieEventType::READY_TO_SLEEP:
+      Serial << "Ready to sleep" << endl;
+      break;
+  }
+}
 
 void setup()
 {
- 
-Serial.begin(115200);
-  Serial << endl << endl;
+  Homie_setFirmware("", "1.0.0"); 
+  Homie.onEvent(onHomieEvent);
+  Homie.setLedPin(16, false);
+  Serial.begin(115200);
+    Serial.println("Starting Heizungscontroller");
+    Serial.flush();
+    Homie.disableResetTrigger();
+    Homie.setup();
+    Serial.begin(115200);
+    Serial.println("Finished Setup");
+    Serial.flush();
   lcd.init();                      // initialize the lcd 
   lcd.backlight();
 
@@ -41,8 +95,6 @@ Serial.begin(115200);
   // WORLD.
   lcd.setCursor(5, 1);      
   lcd.print("mi zadek");
-  Homie_setFirmware("awesome-relay", "1.0.0"); 
-   Homie.setup();
 }
 
  void loop(void) {
